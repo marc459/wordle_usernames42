181501d898a0c78cf16a40881ae7f5d10b3a929f
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2022-04-04 19:51:56 +0200

    server gets usersdata and madrid campus from api

--- backend/server.py
+++ backend/server.py
@@ -20,20 +20,21 @@ payload = {
 
 
 
+
+
+
 ic.progress_bar=True
 #usersdata = ic.pages_threaded("users", params=payload)
-usersdata = ic.get("users")
+usersdata = ic.pages_threaded("users", params=payload)
+#usersdata = ic.get("users")
 #teamsdata = ic.pages_threaded("campus", params=payload2)
 teamsdata = ic.get("campus")
 
 @app.route("/api/users", methods=["GET"])
-def get_users():
-    if usersdata.status_code == 200:
-        data = usersdata.json()
-        return json.dumps(data)
-
+def get_users() -> {}:
+    json_string = json.dumps(usersdata)
+    return json_string
             
-
 @app.route("/api/campus", methods=["GET"])
 def get_teams():
     if teamsdata.status_code == 200:
