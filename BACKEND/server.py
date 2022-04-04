b3989bd83bffe2fe07820069c9d91733fe63d88f
Marcos Santos Serrano <santosserranomarcos@gmail.com>
2022-04-05 00:08:59 +0200

    heroku backend

--- BACKEND/server.py
+++ BACKEND/server.py
@@ -5,6 +5,7 @@ from flask import Flask
 from datetime import date
 from flask_cors import CORS
 
+
 import os
 import sqlite3
 import json
@@ -12,39 +13,36 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-# payload = {
-#     "filter[primary_campus_id]":campus_id
+payload = {
+    "filter[primary_campus_id]":campus_id
+}
+
+# payload2 = {
+#     "filter[name]": "Madrid"
 # }
 
-# # payload2 = {
-# #     "filter[name]": "Madrid"
-# # }
 
+ic.progress_bar=True
+#usersdata = ic.pages_threaded("users", params=payload)
+usersdata = ic.pages_threaded("users", params=payload)
+#usersdata = ic.get("users")
+#teamsdata = ic.pages_threaded("campus", params=payload2)
+teamsdata = ic.get("campus")
 
-
-
-
-
-# ic.progress_bar=True
-# #usersdata = ic.pages_threaded("users", params=payload)
-# usersdata = ic.pages_threaded("users", params=payload)
-# #usersdata = ic.get("users")
-# #teamsdata = ic.pages_threaded("campus", params=payload2)
-# teamsdata = ic.get("campus")
-
-# @app.route("/api/users", methods=["GET"])
-# def get_users() -> {}:
-#     json_string = json.dumps(usersdata)
-#     return json_string
+@app.route("/api/users", methods=["GET"])
+def get_users() -> {}:
+    json_string = json.dumps(usersdata)
+    return json_string
             
-# @app.route("/api/campus", methods=["GET"])
-# def get_teams():
-#     if teamsdata.status_code == 200:
-#         data = teamsdata.json()
-#         return json.dumps(data)
+@app.route("/api/campus", methods=["GET"])
+def get_teams():
+    if teamsdata.status_code == 200:
+        data = teamsdata.json()
+        return json.dumps(data)
 
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5000)
+# if __name__ == "__main__":
+#     app.run(host = "0.0.0.0", port = 5000)
 
-
-
+@app.route("/", methods=["GET"])
+def index():
+    return "<h1>Hello, world!</h1>"
